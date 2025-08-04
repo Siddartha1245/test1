52b17ba6929f39302b3be4f23ff8799d0033ef75
Siddartha1245 <spaloju@gitam.in>
2025-08-04 14:57:19 +0530

    Update se1(p1).py

--- se1(p1).py
+++ se1(p1).py
@@ -1,10 +1,10 @@
-a=1
-b=-3
-c=2
+a=10
+b=-30
+c=20
 d=(b*b)-(4*a*c)
 if d>=0:
     r1=(-b+d**0.5)/(2*a)
     r2=(-b-d**0.5)/(2*a)
-    print("The roots are:",r1,r2)
+    print("The real roots are:",r1,r2)
 else:
-    print("No real roots")
+    print("No real roots found")
